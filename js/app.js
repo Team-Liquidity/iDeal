5157ccd28f30dd97d89b37ba92e7f8dd8fff8a85
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-04-16 22:53:27 +0300

    talafha code

--- js/app.js
+++ js/app.js
@@ -22,24 +22,48 @@ for (let i = 0; i < addButton.length; i++) {
         let total = document.getElementsByClassName('total')[0];
         total.textContent = counter;
         totalPrice+=getQuantity*getPrice;
-        console.log(getQuantity);
+        updateStorage();
+        console.log(pictArr);
+        /*console.log(getQuantity);
         console.log(getPrice);
-        console.log(totalPrice);
-        
-
+        console.log(totalPrice);*/
     }
+
 }
+//################################################################
+function updateStorage() {
+    let arrSring;
+        for (let i = 0; i < pictArr.length; i++) {
+        arrSring =(pictArr[i]);
+        
+    }
+    
+    localStorage.setItem('products',arrSring)
+}
+//###############################################################    
+function getProducts() {
+    let data = localStorage.getItem('products');
+    let productsdata=JSON.parse(data);
+    if (productsdata!==null) {
+        pictArr=productsdata;
+        
+    }
+    return productsdata;
+    
+}
+//let productsdata=getProducts();
+//getProducts();
+//console.log(productsdata);
+//################################################################
+
  function cartRendering () {
      let productsEl = document.getElementById('cart');
-     
-     
-
-     
-
-
-     
+     let imagelement =document.createElement ('image');
+     productsEl.appendChild(imagelement);
+     imagelement.textContent=localStorage.getItem('products');
+      
  }
- console.log(pictArr);
+
  cartRendering();
 
 // let buttonCart = document.getElementById('moveToCart');
