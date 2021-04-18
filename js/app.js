2d0a9fb93c54b78779ec3537adc5f461009cff6f
Baraah Alomari <balomari995@gmail.com>
2021-04-18 20:30:21 +0300

    adding table

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ let addButton = document.getElementsByTagName('button');
 let counter = 0;
 let totalPrice = 0;
 let pictArr = [];
+let srcImg = [];
+let quantityArr =[];
+let priceArr=[];
+let getQuantity=0;
+
+
 
 for (let i = 0; i < addButton.length; i++) {
     addButton[i].addEventListener('click', addToCart);
@@ -15,8 +21,14 @@ for (let i = 0; i < addButton.length; i++) {
         console.log(img);
         pictArr.push(img); //add the product picture to the pictArr
         console.log(pictArr);
-        let getQuantity = document.getElementsByClassName('quantity')[i].value; // gets the quantity of bought products
+        //   srcImg.push(img.src);
+           console.log(pictArr);
+        getQuantity = document.getElementsByClassName('quantity')[i].value; // gets the quantity of bought products
+        pictArr.push(getQuantity);
+        quantityArr.push(getQuantity);
+        console.log(pictArr);
         let getPrice = price[i].getAttribute('value'); //price per product
+        priceArr.push(getPrice);
         counter += parseInt(getQuantity); // parse the value to an integer
         let total = document.getElementsByClassName('total')[0];
         total.textContent = counter;
@@ -32,15 +44,22 @@ for (let i = 0; i < addButton.length; i++) {
 //################################################################
 function updateStorage() {
     let arrString;
-        arrString = JSON.stringify(pictArr);
-        localStorage.setItem('products', arrString);
-        let totalP = totalPrice;
-        localStorage.setItem('total' , JSON.stringify(totalP));
+    let quantityString;
+    let priceString;
+    arrString = JSON.stringify(pictArr);
+    quantityString=JSON.stringify(quantityArr);
+    priceString=JSON.stringify(priceArr)
+    localStorage.setItem('products', arrString);
+    localStorage.setItem('quantity',quantityString);
+    localStorage.setItem('price',priceString);
+    let totalP = totalPrice;
+    localStorage.setItem('total', JSON.stringify(totalP));
 }
 //###############################################################    
 function getProducts() {
     let data = localStorage.getItem('products');
     let productsdata = JSON.parse(data);
+
     if (productsdata !== null) {
         pictArr = productsdata;
 
@@ -55,23 +74,70 @@ function getProducts() {
 
 function cartRendering() {
     let productsEl = document.getElementById('cart-product');
-    for(let i = 0 ; i < pictArr.length ; i++) {
-        let imagelement = document.createElement('img');
-        imagelement.setAttribute('width' , '200px');
-        imagelement.setAttribute('height' , '200px');
-        productsEl.appendChild(imagelement);
-        imagelement.setAttribute('src' , pictArr[i]);
+    for (let i = 0; i < pictArr.length; i++) {
+
+        let imagElement = document.createElement('img');
+        imagElement.setAttribute('width', '200px');
+        imagElement.setAttribute('height', '200px');
+        productsEl.appendChild(imagElement);
+        imagElement.setAttribute('src', pictArr[i]);
+        srcImg.push(imagElement.src);
+
+      //  console.log(srcImg);
+       /* getbreack:if (srcImg[i] === imagElement.src) {
+            break getbreack;
+        } else {
+            continue;
+        }*/
+      
+        
     }
 }
+
+
+
+
+function renderTable (){
+    let parent = document.getElementById('table-product');
+let table = document.createElement('table');
+parent.appendChild(table);
+
+
+
+let firstRow = document.createElement('tr');
+         table.appendChild(firstRow);
+         let thPrice = document.createElement('th');
+         firstRow.appendChild(thPrice);
+         thPrice.textContent='Price';
+
+         let thProductPrice = document.createElement('th');
+         firstRow.appendChild(thProductPrice);
+         thProductPrice.textContent=localStorage.getItem('price');
+
+let secondRow = document.createElement('tr');
+         table.appendChild(secondRow);
+         secondRow.textContent='Quantity';
+
+
+}
+
+
+
+renderTable();
+
+
+
+
+
 getProducts();
 cartRendering();
 
 
 
 let purchaseForm = document.getElementById('cart-form');
-purchaseForm.addEventListener('submit' , createInvoice);
+purchaseForm.addEventListener('submit', createInvoice);
 
-function createInvoice (event) {
+function createInvoice(event) {
     event.preventDefault();
     let name = event.target['name'].value;
     let phoneNumber = event.target['number'].value;
@@ -79,11 +145,10 @@ function createInvoice (event) {
     let payPal = event.target['paypal'].checked;
     let cash = event.target['cash'].checked;
     let visa = event.target['visa'].checked;
-    
+
     renderInvoice();
 }
 
 
-function renderInvoice(){
-    
-}
+// function renderInvoice(){
+    //}
