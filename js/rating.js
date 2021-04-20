b9a13181f71cffd2fb2076027990f76576f855ab
mmohiesen96 <mmohiesen996@gmail.com>
2021-04-21 01:24:32 +0300

    Finished star rating

--- js/rating.js
+++ js/rating.js
@@ -1,4 +1,3 @@
-let avgRating = [];
 
 let st5 = document.getElementById('star5');
 let st4 = document.getElementById('star4');
@@ -12,46 +11,47 @@ st3.addEventListener('change', UserClick);
 st2.addEventListener('change', UserClick);
 st1.addEventListener('change', UserClick);
 
-
+let averageArr = [];
 function UserClick(event) {
-console.log(event);
-let avgRating = [];
-console.log(avgRating);
+    console.log(event);
+    console.log(averageArr);
     if (event.target.id === 'star5') {
-        avgRating.push('5');
-
+        averageArr.push(5);
     }
     else if (event.target.id === 'star4') {
-        avgRating.push('4');
+        averageArr.push(4);
     }
     else if (event.target.id === 'star3') {
-        avgRating.push('3');
+        averageArr.push(3);
+
     }
     else if (event.target.id === 'star2') {
-        avgRating.push('2');
+        averageArr.push(2);
+
     }
     else if (event.target.id === 'star1') {
-        avgRating.push('1');
+        averageArr.push(1);
     } else {
         alert('No Rating yet!');
     }
-    
+
     updateStorage();
 }
 
+// let avgRating1 = [];
+// console.log(avgRating1);
+
 function updateStorage() {
-    
-    localStorage.setItem('rating' , JSON.stringify(avgRating));   
-         
+    localStorage.setItem('rating', JSON.stringify(averageArr));
 }
 function getlocalStorage() {
     let avgString = localStorage.getItem('rating');
     let avgArr = JSON.parse(avgString);
-     if(avgRating){
-         avgRating =avgArr;
-     }
-        
+    if (avgArr) {
+        averageArr = avgArr;
+    }
 
-    
+
+
 }
 getlocalStorage();
